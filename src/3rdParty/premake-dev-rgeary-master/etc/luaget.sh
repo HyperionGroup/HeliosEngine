6a274c1ccbf5f4b6ab9c107be28f72e1d7ca6eda
Vazquinhos <vazquinhos@gmail.com>
2017-06-29 16:08:54 +0200

    remove submodules

--- src/3rdParty/premake-dev-rgeary-master/etc/luaget.sh
+++
@@ -1,2 +0,0 @@
-cd $(dirname $(readlink -f "$0"))/../src
-find . -name '*.lua' | xargs grep -E --color=always -n "$1[ ]=="
--- src/3rdParty/premake-dev-rgeary-master/lib/lfs/premake_lfs.lua
+++
@@ -1,7 +0,0 @@
-
-lib "lfs"
-	files "src/lfs.c"
-	
-	configuration "windows"
-		linkflags "/def:src\lfs.def"
-	
